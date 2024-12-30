5429ea695213d8ab3fdd239f8b491e8d3354467e
Mattiello, Luca <lucamat90@gmail.com>
2024-12-30 20:21:35 +0100

    Finish 2024/21

--- 2024/21/sol.py
+++ 2024/21/sol.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import functools
 import time
 
 
@@ -19,10 +20,11 @@ class Button:
 
 
 class Keypad:
-    def __init__(self, position):
+    def __init__(self, position, next_keypad, out):
         self.buttons = []
         self.position = position
-        self.out = None
+        self.next_keypad = next_keypad
+        self.out = out
 
     def get_button(self, value):
         for b in self.buttons:
@@ -45,10 +47,29 @@ class Keypad:
         current_button = self.get_button(self.position)
         self.position = current_button.right
 
+    def reset(self, position, out):
+        self.position = position
+        self.out = out
+
+    def press(self, value):
+        self.out += value
+        if self.next_keypad is None:
+            return
+        if value == '^':
+            self.next_keypad.move_up()
+        elif value == '<':
+            self.next_keypad.move_left()
+        elif value == 'v':
+            self.next_keypad.move_down()
+        elif value == '>':
+            self.next_keypad.move_right()
+        elif value == 'A':
+            self.next_keypad.press(self.next_keypad.position)
+
 
 class NumericKeypad(Keypad):
     def __init__(self, position, out):
-        Keypad.__init__(self, position)
+        Keypad.__init__(self, position, None, out)
         self.buttons = [
             Button('0', None, '2', 'A', None),
             Button('1', None, '4', '2', None),
@@ -62,15 +83,11 @@ class NumericKeypad(Keypad):
             Button('9', '8', None, None, '6'),
             Button('A', '0', '3', None, None)
         ]
-        self.out = out
-
-    def press(self, value):
-        self.out += value
 
 
 class DirectionalKeypad(Keypad):
-    def __init__(self, position, next_keypad):
-        Keypad.__init__(self, position)
+    def __init__(self, position, next_keypad, out):
+        Keypad.__init__(self, position, next_keypad, out)
         self.buttons = [
             Button('<', None, None, 'v', None),
             Button('v', '<', '^', '>', None),
@@ -78,72 +95,100 @@ class DirectionalKeypad(Keypad):
             Button('^', None, None, 'A', 'v'),
             Button('A', '^', None, None, '>')
         ]
-        self.next_keypad = next_keypad
-
-    def press(self, value):
-        if value == '^':
-            self.next_keypad.move_up()
-        elif value == '<':
-            self.next_keypad.move_left()
-        elif value == 'v':
-            self.next_keypad.move_down()
-        elif value == '>':
-            self.next_keypad.move_right()
-        elif value == 'A':
-            self.next_keypad.press(self.next_keypad.position)
 
 
-def get_positions(dk, nk):
-    positions = tuple([d.position for d in dk])
-    positions += (nk.position,)
+def get_positions(keypads):
+    positions = tuple([k.position for k in keypads])
     return positions
 
 
-def reset_keypads(positions, out):
-    n_keypads = len(positions)
-    nk = NumericKeypad(positions[n_keypads - 1], out)
-    dk = []
-    for i in range(n_keypads - 1):
-        dk.append(DirectionalKeypad(positions[i], None))
-    for i in range(1, n_keypads - 1):
-        dk[i - 1].next_keypad = dk[i]
-    dk[n_keypads - 2].next_keypad = nk
-    return dk, nk
+def get_outs(keypads):
+    return keypads[0].out, keypads[1].out
 
 
-def find_sequence_length(dk, nk, value):
-    start_positions = get_positions(dk, nk)
+def reset_keypads(keypads, positions, outs):
+    for i, keypad in enumerate(keypads):
+        keypad.position = positions[i]
+        if i < 2:
+            keypad.out = outs[i]
+    return keypads
+
+
+def find_best_first_sequence(value, level):
+    first_keypad = DirectionalKeypad('A', None, '')
+    if value[0].isdigit():
+        first_keypad = NumericKeypad('A', '')
+    keypads = [first_keypad]
+    for l in range(1, level):
+        keypads.append(DirectionalKeypad('A', keypads[l - 1], ''))
+    start_positions = get_positions(keypads)
+    outs = get_outs(keypads)
+    queue = [(0, start_positions, outs)]
     distance = {(start_positions, ''): 0}
-    queue = [(0, start_positions, '')]
     while queue:
         queue.sort()
-        current_distance, current_positions, current_out = queue.pop(0)
+        current_distance, current_positions, current_outs = queue.pop(0)
         for c in ['<', 'v', '>', '^', 'A']:
-            new_dk, new_nk = reset_keypads(current_positions, current_out)
+            keypads = reset_keypads(keypads, current_positions, current_outs)
             try:
-                new_dk[0].press(c)
+                keypads[level - 1].press(c)
             except AttributeError:
                 continue
-            if len(new_nk.out) > len(value):
-                continue
-            if len(new_nk.out) < len(value) and not value.startswith(new_nk.out):
-                continue
-            if new_nk.out == value:
-                return current_distance + 1
-            next_positions = get_positions(new_dk, new_nk)
+            next_positions = get_positions(keypads)
+            next_outs = get_outs(keypads)
             if None in next_positions:
                 continue
-            if (next_positions, new_nk.out) not in distance.keys():
-                distance[(next_positions, new_nk.out)] = current_distance + 1
-                queue.append((current_distance + 1, next_positions, new_nk.out))
+            if len(keypads[0].out) > len(value):
+                continue
+            if 0 < len(keypads[0].out) < len(value) and not value.startswith(keypads[0].out):
+                continue
+            if keypads[0].out == value:
+                return next_outs[1], current_distance + 1
+            if (next_positions, keypads[0].out) not in distance.keys():
+                distance[(next_positions, keypads[0].out)] = current_distance + 1
+                queue.append((current_distance + 1, next_positions, next_outs))
+
+
+def find_substrings(string):
+    substrings = string.split('A')
+    return [s + 'A' for s in substrings[:-1]]
+
+
+@functools.cache
+def decompose(sub):
+    if sub == {'A'}:
+        return {'A': 0}
+    first_seq, _ = find_best_first_sequence(sub, 5)
+    subs_list = [s + 'A' for s in first_seq.split('A')[:-1]]
+    subs = {}
+    for sub in subs_list:
+        if sub not in subs.keys():
+            subs[sub] = 0
+        subs[sub] += 1
+    return subs
+
+
+def find_sequence_length(string, levels):
+    seq = {string: 1}
+    for l in range(levels):
+        new_seq = {}
+        for k, v in seq.items():
+            for kk, vv in decompose(k).items():
+                if kk not in new_seq.keys():
+                    new_seq[kk] = 0
+                new_seq[kk] += vv * v
+        seq = new_seq
+    length = 0
+    for k, v in seq.items():
+        length += len(k) * v
+    return length
 
 
 def sol1(filename):
     lines = get_input(filename)
     tot = 0
     for line in lines:
-        dk, nk = reset_keypads(('A',) * 4, '')
-        length = find_sequence_length(dk, nk, line)
+        _, length = find_best_first_sequence(line, 4)
         value = int(line[:-1])
         tot += length * value
     return tot
@@ -153,11 +198,8 @@ def sol2(filename):
     lines = get_input(filename)
     tot = 0
     for line in lines:
-        dk, nk = reset_keypads(('A',) * 8, '')
-        length = find_sequence_length(dk, nk, line)
         value = int(line[:-1])
-        tot += length * value
-        print(length, value)
+        tot += value * find_sequence_length(line, 26)
     return tot
 
 
